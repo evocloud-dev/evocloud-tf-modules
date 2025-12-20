24083aaf76d639c9884b8929364fa75a23bf584f
geanttechnology <geanttechnology@gmail.com>
2025-12-19 20:48:49 -0500

    Added support for aws_vpc resource

---
+++ aws-provider-modules/aws_vpc_resource/outputs.tf
@@ -0,0 +1,22 @@
+#--------------------------------------------------
+# Expose AWS_VPC Attributes
+#--------------------------------------------------
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = try(aws_vpc.this.arn, null)
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = try(aws_vpc.this.id, null)
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = try(aws_vpc.this.cidr_block, null)
+}
+
+output "vpc_instance_tenancy" {
+  description = "Tenancy of instances spin up within VPC"
+  value       = try(aws_vpc.this.instance_tenancy, null)
+}
